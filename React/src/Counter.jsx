fccece2e635144aca622c668f611d549fe0a320b
Vjeko <vjeko@vjeko.com>
2019-11-11 12:09:14 +0100

    Object state

--- React/src/Counter.jsx
+++ React/src/Counter.jsx
@@ -4,16 +4,18 @@ import styles from "./Counter.module.css";
 export class Counter extends Component {
     constructor(props) {
         super(props);
-        this.state = 0;
+        this.state = {
+            counter: 0
+        };
     }
 
     render() {
         return (
             <div className={styles.control}>
-                <div className={styles.counter}>{this.state}</div>
+                <div className={styles.counter}>{this.state.counter}</div>
                 <div className={styles.buttons}>
-                    <div onClick={() => this.state++}>+</div>
-                    <div onClick={() => this.state--}>-</div>
+                    <div onClick={() => this.state.counter++}>+</div>
+                    <div onClick={() => this.state.counter--}>-</div>
                 </div>
             </div>
         )
