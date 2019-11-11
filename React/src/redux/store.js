695eb975e2ed9a10e8900a7087b3a9ff99b0b6c3
Vjeko <vjeko@vjeko.com>
2019-11-11 12:39:29 +0100

    Fixing crappy "standardjs" code

--- React/src/redux/store.js
+++ React/src/redux/store.js
@@ -3,11 +3,11 @@ import { createStore } from "redux";
 function counter(state = 0, action) {
     switch (action.type) {
         case "INCREMENT":
-            return state + 1
+            return state + 1;
         case "DECREMENT":
-            return state - 1
+            return state - 1;
         default:
-            return state
+            return state;
     }
 }
 
