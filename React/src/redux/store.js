08806ff157a08bb21cf15fa6f9ee7828fb5de9a1
Vjeko <vjeko@vjeko.com>
2019-12-03 22:56:31 +0100

    Update

--- React/src/redux/store.js
+++ React/src/redux/store.js
@@ -2,6 +2,15 @@ import { createStore, combineReducers } from "redux";
 import { counter } from "./counter.reducer";
 import { todo } from "./todo.reducer";
 
+export function createReducer(initialState, handlers) {
+    return (state = initialState, action) => {
+        const { type, payload } = action;
+        return handlers[type]
+            ? handlers[type](state, payload)
+            : state;
+    }
+};
+
 export const store = createStore(
     combineReducers({
         counter,
