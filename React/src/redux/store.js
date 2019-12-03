e53e33ebf90697ca13f55aca1d05d0ba5ee1ac54
Vjeko <vjeko@vjeko.com>
2019-12-03 23:58:24 +0100

    Update

--- React/src/redux/store.js
+++ React/src/redux/store.js
@@ -15,6 +15,5 @@ export const store = createStore(
     combineReducers({
         counter,
         todo
-    }),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    })
 );
