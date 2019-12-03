b30ed2a4363e69feb7e6bf1b3b23b1d7bea5f097
Vjeko <vjeko@vjeko.com>
2019-12-03 22:58:25 +0100

    Update

--- React/src/redux/counter.reducer.js
+++ React/src/redux/counter.reducer.js
@@ -1,12 +1,7 @@
 import { COUNTER_ACTIONS } from "./counter.actions";
+import { createReducer } from "./store";
 
-export const counter = (state = 0, action) => {
-    switch (action.type) {
-        case COUNTER_ACTIONS.INCREMENT:
-            return state + 1;
-        case COUNTER_ACTIONS.DECREMENT:
-            return state - 1;
-        default:
-            return state;
-    }
-};
+export const counter = createReducer(0, {
+    [COUNTER_ACTIONS.INCREMENT]: state => state + 1,
+    [COUNTER_ACTIONS.DECREMENT]: state => state -1,
+});
--- React/src/redux/todo.reducer.js
+++ React/src/redux/todo.reducer.js
@@ -1,18 +1,13 @@
 import { TODO_ACTIONS } from "./todo.actions";
+import { createReducer } from "./store";
 
-export const todo = (state = [], action) => {
-    switch (action.type) {
-        case TODO_ACTIONS.ADD:
-            return [...state, action.todo];
-        case TODO_ACTIONS.REMOVE:
-            return state.filter(todo => todo.id !== action.todo.id);
-        case TODO_ACTIONS.TOGGLE:
-            const newState = [...state];
-            const todo = newState.find(todo => todo.id === action.todo.id);
-            if (todo)
-                todo.completed = !todo.completed;
-            return newState;
-        default:
-            return state;
-    }
-};
+export const todo = createReducer([], {
+    [TODO_ACTIONS.ADD]: (state, payload) => [...state, payload],
+
+    [TODO_ACTIONS.REMOVE]: (state, payload) => state.filter(todo => todo.id !== payload.id),
+
+    [TODO_ACTIONS.TOGGLE]: (state, payload) =>
+        state.map(todo => todo.id === payload.id
+            ? { ...todo, completed: !todo.completed }
+            : todo)
+});
